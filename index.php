571a4d4123df67a31e41b82196a6d7d5959e1a03
luis-sardinha <nunes.08.lns@gmail.com>
2021-11-24 14:24:55 -0300

    alteração

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+<h1>Bem vindo a tela de login</h1>
+
+
+<p>Digite seu login e senha</p>
+
+<?php
+
+echo "olá mundo";
+
+$nome = "luis";
+
+
+
+
+?>
+
+   
+</body>
+</html>
